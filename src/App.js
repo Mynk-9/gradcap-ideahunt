0c1b166223e6c9cd0fe8e8aa5f44066c56d96910
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-01-28 03:13:19 +0530

    Added routing and /idea-panel route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import './App.scss';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import IdeaPanel from './pages/ideapanel/IdeaPanel';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <>
+            <Navbar />
             <Routes>
                 <Route exact path="/" element={<></>} />
+                <Route path="/idea-panel" element={<IdeaPanel />} />
             </Routes>
-        </BrowserRouter>
+            <Footer />
+        </>
     );
 };
 
---
+++ src/pages/ideapanel/IdeaPanel.js
@@ -0,0 +1,11 @@
+import Styles from './IdeaPanel.module.scss';
+
+const IdeaPanel = () => {
+    return (
+        <div className={`page-section ${Styles.main}`}>
+            <h1>{'Idea Panel'}</h1>
+        </div>
+    );
+};
+
+export default IdeaPanel;
