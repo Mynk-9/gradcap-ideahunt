1238db818455e45c5a28114a5ef9afbef10aa84d
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-03 21:09:43 +0530

    Implementation of IdeaPanel without api connection

--- src/pages/ideapanel/IdeaPanel.js
+++ src/pages/ideapanel/IdeaPanel.js
@@ -1,10 +1,92 @@
+import { useNavigate } from 'react-router-dom';
+
+import PrimarySection from '../../components/primarysection/PrimarySection';
+import IdeaPanelAccordion from '../../components/ideapanelaccordion/IdeaPanelAccordion';
+import Pagination from '../../components/pagination/Pagination';
+
 import Styles from './IdeaPanel.module.scss';
 
+import UserIcon from './../../assets/icons/user-profile.svg';
+import Img1 from './../../assets/images/discovery.svg';
+import Img2 from './../../assets/images/blogging.svg';
+import Img3 from './../../assets/images/idea-2.svg';
+import Img4 from './../../assets/images/special-deals.svg';
+
+const dummyIdea = {
+    profile: UserIcon,
+    heading: 'Consumable plastic for saving the environment',
+    likes: 50,
+    comments: 5,
+    details:
+        'This idea can be developed by biologists and has been in talks for some time now, you can share your insights over this. This idea can be developed by biologists and has been in talks for some time now, you can share your insights over this.',
+};
+
+const dummyIdeas = [
+    dummyIdea,
+    dummyIdea,
+    dummyIdea,
+    dummyIdea,
+    dummyIdea,
+    dummyIdea,
+    dummyIdea,
+    dummyIdea,
+];
+
 const IdeaPanel = () => {
+    let navigate = useNavigate();
+
     return (
-        <div className={`page-section ${Styles.main}`}>
-            <h1>{'Idea Panel'}</h1>
-        </div>
+        <PrimarySection hero={false}>
+            <h1 className={Styles.heading}>{'Idea Panel'}</h1>
+            <div className={Styles.pageColumnWrapper}>
+                <div className={Styles.pageColumn}>
+                    <div className={Styles.toolsPanel}>
+                        <select name="sort" id="idea-panel-sorting">
+                            <option value={'Featured'}>{'Featured'}</option>
+                            <option value={'Newest'}>{'Newest'}</option>
+                            <option value={'Most Liked'}>{'Most Liked'}</option>
+                        </select>
+                        <button
+                            className={Styles.postIdea}
+                            onClick={() => navigate('/post-idea')}
+                        >
+                            {'Post your idea'}
+                        </button>
+                    </div>
+                    <div className={Styles.ideaListWrapper}>
+                        {/* <IdeaPanelAccordion idea={dummyIdea} /> */}
+                        {dummyIdeas.map((idea, i) => (
+                            <IdeaPanelAccordion
+                                idea={idea}
+                                key={`${idea.heading}-${i}`}
+                            />
+                        ))}
+                    </div>
+                    <Pagination
+                        totalPages={10}
+                        onPageChange={newPage =>
+                            console.log('newPage', newPage)
+                        }
+                    />
+                </div>
+                <div className={Styles.pageColumn}>
+                    <img src={Img1} />
+                    <img src={Img2} />
+                    <img src={Img3} />
+                </div>
+            </div>
+            <div className={Styles.rewards}>
+                <div className={Styles.message}>
+                    <h1 className={Styles.heading}>{'Rewards'}</h1>
+                    <p>
+                        {
+                            'We offer a number of rewards for some of the best ideas that we carefully study and curate!'
+                        }
+                    </p>
+                </div>
+                <img src={Img4} />
+            </div>
+        </PrimarySection>
     );
 };
 
