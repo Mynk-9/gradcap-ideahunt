af5834352db7ec531ec96eba81c1960b3d4da6b2
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-03 21:27:16 +0530

    Finalized navbar UI

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,18 @@
 import Styles from './Navbar.module.scss';
 
+import IdeaHuntLogo from './../../assets/icons/ideaHuntLogo.png';
+import UserIcon from './../../assets/icons/user-profile.svg';
+
 const Navbar = () => {
     return (
         <nav className={Styles.nav}>
-            <div className={Styles.item}>{'IDEA HUNT'}</div>
-            <div className={Styles.item}>{'Contact Us'}</div>
+            <div className={Styles.item}>
+                <img src={IdeaHuntLogo} alt={'Idea Hunt Logo'} />
+            </div>
+            <div className={Styles.item}>
+                <span>{'Contact Us'}</span>
+                <img src={UserIcon} alt={'User profile'} />
+            </div>
         </nav>
     );
 };
