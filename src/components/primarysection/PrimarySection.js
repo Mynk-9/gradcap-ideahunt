c516445715af574c4ca110f06595106088371fd9
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-03 21:09:00 +0530

    Implemented component for wrapping page's main contents

--- src/components/primarysection/PrimarySection.js
+++ src/components/primarysection/PrimarySection.js
@@ -2,8 +2,12 @@ import PropTypes from 'prop-types';
 
 import Styles from './PrimarySection.module.scss';
 
-const PrimarySection = ({ children }) => {
-    return <div className={Styles.primarySection}>{children}</div>;
+const PrimarySection = ({ children, hero }) => {
+    return (
+        <div className={`${Styles.primarySection} ${hero ? Styles.hero : ''}`}>
+            {children}
+        </div>
+    );
 };
 
 PrimarySection.propTypes = {
@@ -11,6 +15,7 @@ PrimarySection.propTypes = {
         PropTypes.arrayOf(PropTypes.node),
         PropTypes.node,
     ]).isRequired,
+    hero: PropTypes.bool,
 };
 
 export default PrimarySection;
