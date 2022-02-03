44a59e02806cdd4ca295421c0a18c41582e227d1
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-03 21:06:17 +0530

    Implemented footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,23 @@
 import Styles from './Footer.module.scss';
 
+import GradCapitalLogo from './../../assets/icons/gradCapitalLogo.png';
+import TwitterIcon from './../../assets/icons/twitter.svg';
+import InstagramIcon from './../../assets/icons/insta.svg';
+import LinkedinIcon from './../../assets/icons/linkedin.svg';
+
 const Footer = () => {
     return (
         <footer className={Styles.footer}>
-            <div className={Styles['section-address']}></div>
-            <div className={Styles['section-links']}></div>
+            <div className={Styles.sectionAddress}>
+                <div>{'Q-58, Diamond District, Domlur'}</div>
+                <div>{'Bengaluru, 560008'}</div>
+            </div>
+            <div className={Styles.sectionLinks}>
+                <img className={Styles.logo} src={GradCapitalLogo} />
+                <img src={TwitterIcon} />
+                <img src={LinkedinIcon} />
+                <img src={InstagramIcon} />
+            </div>
         </footer>
     );
 };
