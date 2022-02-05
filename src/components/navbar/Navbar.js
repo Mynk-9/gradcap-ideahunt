408ce2e3b591b3735801bdd4185895078c0e971f
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-06 04:25:23 +0530

    Navbar made responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,15 +2,21 @@ import Styles from './Navbar.module.scss';
 
 import IdeaHuntLogo from './../../assets/icons/ideaHuntLogo.png';
 import UserIcon from './../../assets/icons/user-profile.svg';
+import Hamburger from './../../assets/icons/hamburger.svg';
 
 const Navbar = () => {
     return (
         <nav className={Styles.nav}>
-            <div className={Styles.item}>
+            <div className={Styles.item} data-mobile-only>
+                <img src={Hamburger} alt={'Side Menu'} onClick={() => ({})} />
+            </div>
+            <div className={`${Styles.item} ${Styles.logo}`}>
                 <img src={IdeaHuntLogo} alt={'Idea Hunt Logo'} />
             </div>
-            <div className={Styles.item}>
+            <div className={Styles.item} data-pc-only>
                 <span>{'Contact Us'}</span>
+            </div>
+            <div className={`${Styles.item} ${Styles.userProfile}`}>
                 <img src={UserIcon} alt={'User profile'} />
             </div>
         </nav>
