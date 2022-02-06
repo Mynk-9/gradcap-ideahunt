85cd44bfccd78df134cf1b13f0c383a5499a22b2
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-06 06:04:20 +0530

    Navbar made responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,3 +1,8 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import SidebarContext from '../../contexts/SidebarContext';
+
 import Styles from './Navbar.module.scss';
 
 import IdeaHuntLogo from './../../assets/icons/ideaHuntLogo.png';
@@ -5,16 +10,22 @@ import UserIcon from './../../assets/icons/user-profile.svg';
 import Hamburger from './../../assets/icons/hamburger.svg';
 
 const Navbar = () => {
+    const { setActive } = useContext(SidebarContext);
+
     return (
         <nav className={Styles.nav}>
             <div className={Styles.item} data-mobile-only>
-                <img src={Hamburger} alt={'Side Menu'} onClick={() => ({})} />
+                <img
+                    src={Hamburger}
+                    alt={'Side Menu'}
+                    onClick={() => setActive(true)}
+                />
             </div>
             <div className={`${Styles.item} ${Styles.logo}`}>
                 <img src={IdeaHuntLogo} alt={'Idea Hunt Logo'} />
             </div>
             <div className={Styles.item} data-pc-only>
-                <span>{'Contact Us'}</span>
+                <Link to={'/contact-us'}>{'Contact Us'}</Link>
             </div>
             <div className={`${Styles.item} ${Styles.userProfile}`}>
                 <img src={UserIcon} alt={'User profile'} />
