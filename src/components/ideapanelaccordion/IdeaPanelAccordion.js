5a33be9de69c9d1454ed9c0be186e853debe677a
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-03-04 15:17:56 +0530

    Updated like/dislike mechanism of accordions according to updated endpoints

--- src/components/ideapanelaccordion/IdeaPanelAccordion.js
+++ src/components/ideapanelaccordion/IdeaPanelAccordion.js
@@ -56,8 +56,7 @@ const IdeaPanelAccordion = ({ idea, featured }) => {
             .then(resp => {
                 if (resp.status !== 201) console.log('Error in liking idea');
                 else {
-                    if (resp.data.likeStatus) setLikeCount(val => val + 1);
-                    else setLikeCount(val => val - 1);
+                    setLikeCount(resp.data?.likes >= 0 ? resp.data.likes : 0);
                     setLiked(val => !val);
                 }
             })
